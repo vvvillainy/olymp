3d2fce8713d024267a9e46a7a6c7cb27dfccc135
vvvillainy <vdovenkoviktor2004@gmail.com>
2023-10-06 04:55:12 +0300

    add: money_db , commentaries_db

--- freezlance/orders/models.py
+++ freezlance/orders/models.py
@@ -6,11 +6,31 @@ from user_profile.models import Users
 # Create your models here.
 class Orders(models.Model):
     name = models.CharField(max_length=255)
-    worker = models.ForeignKey(Users, on_delete=models.PROTECT, related_name= "worker_name")
-    client = models.ForeignKey(Users, on_delete=models.PROTECT, related_name= "client_name")
+    worker = models.ForeignKey(Users, on_delete=models.PROTECT, related_name="worker_name")
+    client = models.ForeignKey(Users, on_delete=models.PROTECT, related_name="client_name")
     price = models.FloatField()
     description = models.TextField()
     status = models.ForeignKey('Status', on_delete=models.PROTECT)
 
-class Status (models.Model):
+
+class Status(models.Model):
     name = models.CharField(max_length=255)
+
+
+'''
+id и статусы
+1) Опубликован
+2)В работе
+3)Отменён
+4)На доработке
+5)Выполнен
+6)Удалён
+7)На проверке
+'''
+
+
+class Commentaries(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.PROTECT)
+    number = models.IntegerField()
+    text = models.TextField()
+    version = models.FileField(upload_to=".")
--- freezlance/user_profile/models.py
+++ freezlance/user_profile/models.py
@@ -9,3 +9,10 @@ class Users(models.Model):
     rating = models.IntegerField(default=0)
     job = models.BooleanField()
     balance = models.FloatField(default=0)
+
+
+class Money(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.PROTECT)
+    where = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    sum = models.IntegerField()
