fb00af59926958c4eb46ce721569bf6a1cad23c2
HlpMe_Sfox <oaillbia@gmail.com>
2023-10-06 03:04:20 -0700

    add: test app

--- freezlance/freezlance/urls.py
+++ freezlance/freezlance/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from orders.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls')),
 ]
