32cc661cbfb235dfbcb34ebac5eec403b441155e
vvvillainy <vdovenkoviktor2004@gmail.com>
2023-10-06 04:22:16 +0300

    add: user_db, status_db, orders_db

--- freezlance/orders/models.py
+++ freezlance/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from user_profile.models import Users
+
+
 # Create your models here.
+class Orders(models.Model):
+    name = models.CharField(max_length=255)
+    worker = models.ForeignKey(Users, on_delete=models.PROTECT, related_name= "worker_name")
+    client = models.ForeignKey(Users, on_delete=models.PROTECT, related_name= "client_name")
+    price = models.FloatField()
+    description = models.TextField()
+    status = models.ForeignKey('Status', on_delete=models.PROTECT)
+
+class Status (models.Model):
+    name = models.CharField(max_length=255)
--- freezlance/user_profile/models.py
+++ freezlance/user_profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    rating = models.IntegerField(default=0)
+    job = models.BooleanField()
+    balance = models.FloatField(default=0)
