48d6bb0c859ef195e49e954463440004842d7bac
HlpMe_Sfox <oaillbia@gmail.com>
2023-10-06 03:33:56 -0700

    add: second test app

--- freezlance/freezlance/urls.py
+++ freezlance/freezlance/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from orders.views import *
 from django.urls import path, include
+from orders.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('orders/', include('orders.urls')),
+    path('user/', include('user_profile.urls')),
 ]
